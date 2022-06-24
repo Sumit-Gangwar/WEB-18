533886abc37f07e9716630397be41ec879b679bc
Sumit-Gangwar <gangwarsumit999@gmail.com>
2022-06-24 14:27:23 +0530

    unit-4 Java

---
+++ unit-4/src/sprint_1/day_1/Details.java
@@ -0,0 +1,7 @@
+package sprint_1.day_1;
+
+public class Details {
+    public static void main(String[] args) {
+        System.out.println("Name : Sumit Gangwar\nFather's Name : Anand Gangwar\nMother's Name : Deepmala\nAge : 20\nGender : Male\nAddress : Shastri Nagar, Bareilly, U.P.\nMobile No. : 8171108970");
+    }
+}
